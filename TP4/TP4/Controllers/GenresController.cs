4fc1b69876b41f5c668131f5a600e934ac2fcf35
Anas Boukchina <mohamedanas.boukchina@carnet.tn>
2023-12-03 14:13:56 +0100

    ajout de tp4

---
+++ TP4/TP4/Controllers/GenresController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TP4.Models;
+
+namespace TP4.Controllers
+{
+    public class GenresController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public GenresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // get genre
+        public async Task<IActionResult> Index()
+        {
+              return _context.Genres != null ? 
+                          View(await _context.Genres.ToListAsync()) :
+                          Problem("Entity set 'AppDbContext.Genres'  is null.");
+        }
+
+        // get genre detail
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // create genre
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // create genre
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,GenreName")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(genre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        // edit genre
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GenreName")] Genre genre)
+        {
+            if (id != genre.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(genre);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GenreExists(genre.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // delete genre
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Genres == null)
+            {
+                return Problem("Entity set 'AppDbContext.Genres'  is null.");
+            }
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre != null)
+            {
+                _context.Genres.Remove(genre);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GenreExists(int id)
+        {
+          return (_context.Genres?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TP4/TP4/Controllers/MembershipTypesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TP4.Models;
+
+namespace TP4.Controllers
+{
+    public class MembershipTypesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MembershipTypesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+              return _context.MembershipTypes != null ? 
+                          View(await _context.MembershipTypes.ToListAsync()) :
+                          Problem("Entity set 'AppDbContext.MembershipTypes'  is null.");
+        }
+
+        // get membershipdetails
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.MembershipTypes == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+
+            return View(membershipType);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // create membership
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SignupFee,DurationInMonths,DiscountRate")] MembershipType membershipType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(membershipType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(membershipType);
+        }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.MembershipTypes == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipTypes.FindAsync(id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+            return View(membershipType);
+        }
+
+        // edit membership
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SignupFee,DurationInMonths,DiscountRate")] MembershipType membershipType)
+        {
+            if (id != membershipType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(membershipType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MembershipTypeExists(membershipType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(membershipType);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.MembershipTypes == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+
+            return View(membershipType);
+        }
+
+        // delete membership
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.MembershipTypes == null)
+            {
+                return Problem("Entity set 'AppDbContext.MembershipTypes'  is null.");
+            }
+            var membershipType = await _context.MembershipTypes.FindAsync(id);
+            if (membershipType != null)
+            {
+                _context.MembershipTypes.Remove(membershipType);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MembershipTypeExists(int id)
+        {
+          return (_context.MembershipTypes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
