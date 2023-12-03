fca647654e7c4ce69b9ce9e37cbab780fd9a8717
Anas Boukchina <mohamedanas.boukchina@carnet.tn>
2023-12-03 14:12:05 +0100

    ajout de tp3

---
+++ tp3/tp3/Controllers/CustomersController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TP3net.Models;
+
+namespace TP3net.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CustomersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // get customers
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Customers.Include(c => c.Membership);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // get customer detail by id
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Membership)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // customer creation
+        public IActionResult Create()
+        {
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Customers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,MembershipTypeId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "Id", "Id", customer.MembershipTypeId);
+            return View(customer);
+        }
+
+        // get customer edits by id
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "Id", "Id", customer.MembershipTypeId);
+            return View(customer);
+        }
+
+        // post customer edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,MembershipTypeId")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "Id", "Id", customer.MembershipTypeId);
+            return View(customer);
+        }
+
+        // delete customer edit by id
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .Include(c => c.Membership)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("Entity set 'AppDbContext.Customers'  is null.");
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+          return (_context.Customers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
